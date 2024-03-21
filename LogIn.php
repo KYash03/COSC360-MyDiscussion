28821c1dd4c8ef49f390af6b2ccef6fedba3df17
YASH KUMAR <kyash03@student.ubc.ca>
2024-03-21 15:11:00 -0700

    added browser state code

--- LogIn.php
+++ LogIn.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'path/to/config/db_config.php';
+// require_once 'path/to/config/db_config.php';
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['username'], $_POST['password'])) {
