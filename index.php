8dd5c7a2dfbe692e4e5cc8b81166182cde8a3f6c
YASH KUMAR <kyash03@student.ubc.ca>
2024-03-23 18:19:11 -0700

    cleaning up the url

--- index.php
+++ index.php
@@ -5,4 +5,8 @@ $validPages = ['home', 'about', 'contact'];
 $requestUri = $_SERVER['REQUEST_URI'];
 $scriptName = $_SERVER['SCRIPT_NAME'];
 
+// cleaning the url
+$path = str_replace($scriptName, '', $requestUri);
+$path = trim($path, '/');
+
 ?>
