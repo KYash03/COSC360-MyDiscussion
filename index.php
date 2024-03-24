3834ebf2846c0ab96348e324e14bcf123faae0fa
YASH KUMAR <kyash03@student.ubc.ca>
2024-03-23 18:18:54 -0700

    requesting page details

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 // IMPORTANT: valid pages (we will need to change this)
 $validPages = ['home', 'about', 'contact'];
+
+$requestUri = $_SERVER['REQUEST_URI'];
+$scriptName = $_SERVER['SCRIPT_NAME'];
+
 ?>
