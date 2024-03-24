8e5b4373495aff417f869bb487f2721dd36b2e7c
YASH KUMAR <kyash03@student.ubc.ca>
2024-03-24 04:13:53 -0700

    added html to 404.php

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Not Found</title>
+</head>
+<body>
+    <h1>Oops! Page Not Found.</h1>
+    <a href="/">Go to Homepage</a>
+</body>
+</html>
